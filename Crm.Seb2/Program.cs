2465b223e8b565ac8dc206d36829404102cc1a14
atrostnikov <A.Trostnikov@sirena-travel.ru>
2018-06-04 20:32:17 +0300

    add nlog and service mode

--- Crm.Seb2/Program.cs
+++ Crm.Seb2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+using WinService;
 
 namespace Crm.Seb2
 {
@@ -10,19 +12,31 @@ namespace Crm.Seb2
     {
         static void Main(string[] args)
         {
-            ServiceSettings ss = ServiceSettings.Instance;
-            System.Console.WriteLine(ss.ConnectionString);
+            /* Logger log = LogManager.GetCurrentClassLogger();
+             log.Trace("trace message");
+             log.Debug("debug message");
+             log.Info("info message");
+             log.Warn("warn message");
+             log.Error("error message");
+             log.Fatal("fatal message");
 
-            foreach (var airline in ss.Airlines)
-            {
-                Console.WriteLine("Airline code: {0}", airline.Code);
-                Console.WriteLine("Airline pathIn: {0}", airline.PathIn);
-                Console.WriteLine();
-            }
+             ServiceSettings ss = ServiceSettings.Instance;
+             System.Console.WriteLine(ss.ConnectionString);
 
-            AirlineElement x = ss.Airlines.SingleOrDefault(a => a.Code == "7R");
-            if (x != null)
-                Console.WriteLine(x.PathIn);
+             foreach (var airline in ss.Airlines)
+             {
+                 Console.WriteLine("Airline code: {0}", airline.Code);
+                 Console.WriteLine("Airline pathIn: {0}", airline.PathIn);
+                 Console.WriteLine();
+             }
+
+             AirlineElement x = ss.Airlines.SingleOrDefault(a => a.Code == "7R");
+             if (x != null)
+                 Console.WriteLine(x.PathIn);*/
+
+            Service S = new Service();
+            ConsoleUtils.RunService(args, S);
+
             Console.ReadKey(true);
         }
     }
---
+++ Crm.Seb2/Service.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinService;
+using NLog;
+using System.Threading;
+
+namespace Crm.Seb2
+{
+    [WindowsService("Crm.Seb2")]
+    public class Service : IWinService
+    {
+        private readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        private Task _proccessSmsQueueTask;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public Service()
+        {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        public void Start()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _proccessSmsQueueTask = Task.Run(() => DoWorkAsync(_cancellationTokenSource.Token));
+        }
+
+        private async Task DoWorkAsync(CancellationToken token)
+        {
+            while (true)
+            {
+                try
+                {
+                    // some work
+                    ServiceSettings ss = ServiceSettings.Instance;
+                    log.Info(ss.ConnectionString);
+
+                    foreach (var airline in ss.Airlines)
+                    {
+                        log.Trace("Airline code: {0}", airline.Code);
+                        log.Trace("Airline pathIn: {0}", airline.PathIn);
+                    }
+                }
+                catch (Exception e)
+                {
+                    log.Fatal(e);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(1), token);
+            }
+        }
+
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+            try
+            {
+                _proccessSmsQueueTask.Wait();
+            }
+            catch (Exception e)
+            {
+                log.Fatal(e);
+            }
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            log.Fatal("Unhandled exception: {0}", e.ExceptionObject);
+            LogManager.Flush();
+            Stop();
+            return;
+        }
+
+
+        /////////////////////////////////////////////////////////////////////////
+
+        // WinService Interface
+
+        /////////////////////////////////////////////////////////////////////////
+
+        public void OnStart(string[] args)
+        {
+            Start();
+        }
+
+        public void OnStop()
+        {
+            Stop();
+        }
+
+        public void OnPause()
+        {
+            Stop();
+        }
+
+        public void OnContinue()
+        {
+            Start();
+        }
+
+        public void OnShutdown()
+        {
+            Stop();
+        }
+
+        public void OnCustomCommand(int command)
+        {
+        }
+
+        /////////////////////////////////////////////////////////////////////////
+
+        public void Dispose()
+        {
+        }
+    }
+}
