38c41c2ad376fe21801094ed3179219240fdf94b
atrostnikov <A.Trostnikov@sirena-travel.ru>
2018-06-04 18:22:46 +0300

    Add project files.

---
+++ Crm.Seb2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crm.Seb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceSettings ss = ServiceSettings.Instance;
+            System.Console.WriteLine(ss.ConnectionString);
+
+            foreach (var airline in ss.Airlines)
+            {
+                Console.WriteLine("Airline code: {0}", airline.Code);
+                Console.WriteLine("Airline pathIn: {0}", airline.PathIn);
+                Console.WriteLine();
+            }
+
+            AirlineElement x = ss.Airlines.SingleOrDefaul
+                t(a => a.Code == "7R");
+            if (x != null)
+                Console.WriteLine(x.PathIn);
+            Console.ReadKey(true);
+        }
+    }
+
+}
---
+++ Crm.Seb2/ServiceSettings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Crm.Seb2
+{
+    public sealed class ServiceSettings : ConfigurationSection
+    {
+        private const string serviceSettingsLabel = "serviceSettings";
+        private const string connectionStringLabel = "connectionString";
+        private const string airlinesLabel = "airlines";
+        private const string airlineLabel = "airline";
+
+        private static readonly Lazy<ServiceSettings> lazy =
+            new Lazy<ServiceSettings>(() => (ServiceSettings)ConfigurationManager.GetSection(serviceSettingsLabel));
+
+        public static ServiceSettings Instance { get { return lazy.Value; } }
+
+        [ConfigurationProperty(connectionStringLabel)]
+        public string ConnectionString
+        {
+            get { return (string)this[connectionStringLabel]; }
+        }
+
+        [ConfigurationProperty(airlinesLabel)]
+        [ConfigurationCollection(typeof(AirlineElementCollection), AddItemName = airlineLabel)]
+        public AirlineElementCollection Airlines
+        {
+            get { return (AirlineElementCollection)this[airlinesLabel]; }
+        }
+    }
+
+    public class AirlineElementCollection : ConfigurationElementCollection, IEnumerable<AirlineElement>
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AirlineElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AirlineElement)element).Code;
+        }
+
+        public new IEnumerator<AirlineElement> GetEnumerator()
+        {
+            foreach (AirlineElement element in
+                Enumerable.Range(0, base.Count).Select(base.BaseGet))
+                yield return element;
+        }
+
+    }
+
+    public class AirlineElement : ConfigurationElement
+    {
+        private const string codeLabel = "code";
+        private const string pathInLabel = "pathIn";
+        private const string pathOutLabel = "pathOut";
+        private const string pathErrLabel = "pathErr";
+
+        [ConfigurationProperty(codeLabel)]
+        public string Code { get { return (string)this[codeLabel]; } }
+
+        [ConfigurationProperty(pathInLabel)]
+        public string PathIn { get { return (string)this[pathInLabel]; } }
+
+        [ConfigurationProperty(pathOutLabel)]
+        public string PathOut { get { return (string)this[pathOutLabel]; } }
+
+        [ConfigurationProperty(pathErrLabel)]
+        public string PathErr { get { return (string)this[pathErrLabel]; } }
+    }
+}
