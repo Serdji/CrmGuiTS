fb8be3bca55c6e181846429be5aa2415278896be
sumatokhinsa <c.c@bk.ru>
2018-06-13 17:04:51 +0300

    refactoring

--- src/app/components/profile-search/profile-search.service.ts
+++ src/app/components/profile-search/profile-search.service.ts
@@ -3,7 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Icity } from '../../interface/icity';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment'
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class ProfileSearchService {
@@ -17,20 +17,20 @@ export class ProfileSearchService {
   }
 
   getTree(): Observable<any> {
-    return this.http.get(environment.crmApi + '/api/api/Segmentation/tree');
+    return this.http.get( environment.crmApi + '/api/api/Segmentation/tree' );
   }
 
   getGroups(): Observable<any> {
-    return this.http.get(environment.crmApi + '/api/api/CustomerGroup/groups');
+    return this.http.get( environment.crmApi + '/api/api/CustomerGroup/groups' );
   }
 
   getProfileSearchCount( params: string ): Observable<any> {
     this.params = params;
-    return this.http.get(environment.crmApi + '/api/api/Customer/quicksearch/count' + params);
+    return this.http.get( environment.crmApi + '/api/api/Customer/quicksearch/count' + params );
   }
 
   getProfileSearch( count: string ): Observable<any> {
-    return this.http.get(environment.crmApi + '/api/api/Customer/quicksearch' + this.params + count);
+    return this.http.get( environment.crmApi + '/api/api/Customer/quicksearch' + this.params + count );
   }
 
 }
