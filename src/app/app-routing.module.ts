51408b6ba5e5680a2188c4528d245cb87f73d4fd
sumatokhinsa <c.c@bk.ru>
2018-06-19 11:06:05 +0300

    login, shared and components module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './page/login/login.component';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth.guard';
-import { ProfileComponent } from './page/profile/profile.component';
 import { ProfileSearchComponent } from './components/profile-search/profile-search.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
     {
       path: 'profile',
-      component: ProfileComponent,
+      component: LayoutComponent,
       canActivate: [ AuthGuard ],
       children: [
         { path: 'profilesearch', component: ProfileSearchComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-import { LoginComponent } from './page/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { AuthGuard } from './auth.guard';
 import { ActivityUserService } from './services/activity-user.service';
-import { DialogComponent } from './shared/dialog/dialog.component';
-import { TableExampleComponent } from './shared/tablet-example/table-example.component';
 import { environment } from '../environments/environment';
-import { LoginService } from './page/login/login.service';
-import { LayoutComponent } from './shared/layout/layout.component';
-import { ToolbarComponent } from './shared/layout/toolbar/toolbar.component';
-import { SidenavComponent } from './shared/layout/sidenav/sidenav.component';
-import { LayoutService } from './shared/layout/layout.service';
-import { ProfileComponent } from './page/profile/profile.component';
 import { ProfileSearchComponent } from './components/profile-search/profile-search.component';
 import { ProfileSearchService } from './components/profile-search/profile-search.service';
 import { AuthInterceptor } from './services/auth-interceptor';
+import { SharedModule } from './shared/shared.module';
 import { TableAsyncComponent } from './shared/table-async/table-async.component';
-import { TableAsyncService } from './shared/table-async/table-async.service';
+import { DialogComponent } from './shared/dialog/dialog.component';
+import { ComponentsModule } from './components/components.module';
 
 
 if ( environment.production ) {
@@ -35,35 +28,27 @@ if ( environment.production ) {
 @NgModule( {
   declarations: [
     AppComponent,
-    LoginComponent,
-    DialogComponent,
-    TableExampleComponent,
-    LayoutComponent,
-    ToolbarComponent,
-    SidenavComponent,
-    ProfileComponent,
     ProfileSearchComponent,
     TableAsyncComponent,
-  ],
-  entryComponents: [
-    DialogComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    ComponentsModule,
+    MaterialModule,
+    SharedModule.forRoot()
+  ],
+  entryComponents: [
+    DialogComponent,
   ],
   providers: [
     AuthService,
     AuthGuard,
     ActivityUserService,
-    LoginService,
-    LayoutService,
     ProfileSearchService,
-    TableAsyncService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
