c95d8ad1a9a4361b20d5310c0624b9313fdf4e9d
sumatokhinsa <c.c@bk.ru>
2018-07-04 17:41:01 +0300

    hot fix validator email

--- src/app/validators/emailValidator.ts
+++ src/app/validators/emailValidator.ts
@@ -1,6 +1,7 @@
 import {FormControl} from '@angular/forms';
 
 export function emailValidator(formControl: FormControl) {
+  if (formControl.value === '') return null;
   const re =  /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/;
   if (re.test(formControl.value)) {
     return null;
