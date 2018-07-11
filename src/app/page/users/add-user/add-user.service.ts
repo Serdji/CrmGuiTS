273450a46a71fe0601ae7268a50deb1ac98accf2
sumatokhinsa <c.c@bk.ru>
2018-07-11 09:31:51 +0300

    retry

--- src/app/page/users/add-user/add-user.service.ts
+++ src/app/page/users/add-user/add-user.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { IcreateUser } from '../../../interface/icreate-user';
+import { retry } from 'rxjs/operators';
 
 @Injectable()
 export class AddUserService {
@@ -10,7 +11,7 @@ export class AddUserService {
   constructor( private httpQuery: HttpClient ) { }
 
   createUser( params: IcreateUser ): Observable<any> {
-    return this.httpQuery.post( environment.crmApi + '/admin/user', params );
+    return this.httpQuery.post( environment.crmApi + '/admin/user', params ).pipe( retry( 10 ) );
   }
 
 }
