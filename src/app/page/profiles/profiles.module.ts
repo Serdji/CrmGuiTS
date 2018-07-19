e87d63d93744095c24614d6f337ea66c39c9be8d
sumatokhinsa <c.c@bk.ru>
2018-07-19 10:05:51 +0300

    Новый компонет добавления пассажиров

--- src/app/page/profiles/profiles.module.ts
+++ src/app/page/profiles/profiles.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileSearchModule } from './profile-search/profile-search.module';
 import { ProfileModule } from './profile/profile.module';
+import { AddProfileModule } from './add-profile/add-profile.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ProfileSearchModule,
-    ProfileModule
+    ProfileModule,
+    AddProfileModule,
   ],
   declarations: []
 })
