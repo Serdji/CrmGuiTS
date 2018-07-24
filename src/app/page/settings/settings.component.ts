e07891130fd27ab9b7ef25d747ae1beb7656e099
sumatokhinsa <c.c@bk.ru>
2018-07-24 15:38:01 +0300

    Чикбоксы для настройки отображения сталбцов формы

--- src/app/page/settings/settings.component.ts
+++ src/app/page/settings/settings.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SettingsService } from './settings.service';
 
-@Component({
+@Component( {
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.styl']
-})
+  styleUrls: [ './settings.component.styl' ]
+} )
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  public formTableAsyncProfile: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.initFormTableAsyncProfile();
+    this.saveParamsTableAsyncProfile();
+  }
+
+  private initFormTableAsyncProfile() {
+    this.formTableAsyncProfile = this.fb.group( {
+      'firstName': '',
+      'lastName': '',
+      'middleName': '',
+      'prefix': '',
+      'gender': '',
+      'dob': '',
+    } );
+    this.isParamsTableAsyncProfile();
+  }
+
+  private isParamsTableAsyncProfile() {
+    for ( const checkbox of JSON.parse( localStorage.getItem( 'tableAsyncProfile' ) ) ) {
+      this.formTableAsyncProfile.patchValue( { [ checkbox ]: true } );
+    }
+  }
+
+  private saveParamsTableAsyncProfile() {
+    this.formTableAsyncProfile.valueChanges.subscribe( checkbox => {
+      const saveTableAsyncProfile = [];
+      for ( const key in checkbox ) {
+        if ( checkbox[ key ] ) {
+          saveTableAsyncProfile.push( key );
+        }
+      }
+      localStorage.setItem( 'tableAsyncProfile', JSON.stringify( saveTableAsyncProfile ) );
+    } );
   }
 
 }
--- src/app/page/settings/settings.module.ts
+++ src/app/page/settings/settings.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './settings.component';
 import { SharedModule } from '../../shared/shared.module';
 import { SettingsService } from './settings.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule( {
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
   ],
   declarations: [ SettingsComponent ],
   providers: [ SettingsService ]
