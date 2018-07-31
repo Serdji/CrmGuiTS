df08e91b394c8a060b2ae9cd02bc8834308a16d7
sumatokhinsa <c.c@bk.ru>
2018-07-31 17:16:57 +0300

    Запросы к API

--- src/app/page/profiles/tabs-profile/contact/contact.component.ts
+++ src/app/page/profiles/tabs-profile/contact/contact.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ContactService } from './contact.service';
+import { takeWhile } from 'rxjs/operators';
+import { Icontact } from '../../../../interface/icontact';
 
-@Component({
+@Component( {
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.styl']
-})
-export class ContactComponent implements OnInit {
+  styleUrls: [ './contact.component.styl' ]
+} )
+export class ContactComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() id: number;
+  private isActive: boolean = true;
 
-  ngOnInit() {
+  constructor( private contactService: ContactService ) { }
+
+  ngOnInit(): void {
+    this.initContact();
+  }
+
+  private initContact() {
+    this.contactService.getContact( 1 )
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( ( value: Icontact[] ) => {
+        console.log( value );
+      } );
+  }
+
+  ngOnDestroy(): void {
+    this.isActive = false;
   }
 
 }
--- src/app/page/profiles/tabs-profile/contact/contact.service.ts
+++ src/app/page/profiles/tabs-profile/contact/contact.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
+import { retry } from 'rxjs/operators';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class ContactService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  addContact( params ): Observable<any> {
+    return this.http.post( environment.crmApi + '/crm/contact', params ).pipe( retry( 10 ) );
+  }
+
+  getContact( id: number ): Observable<any> {
+    return this.http.get( `${environment.crmApi}/crm/customer/${id}/contact` ).pipe( retry( 10 ) );
+  }
+
+  deleteContact( contactId: number ): Observable<any> {
+    return this.http.delete( `${environment.crmApi}/crm/contact/${contactId}` ).pipe( retry( 10 ) );
+  }
+
 }
