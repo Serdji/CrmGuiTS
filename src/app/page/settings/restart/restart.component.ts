ff7b4c149c39d0c8387ff6ad74413e2d5726b630
sumatokhinsa <c.c@bk.ru>
2018-08-13 10:45:37 +0300

    Перезагрузка приложения

--- src/app/page/settings/restart/restart.component.ts
+++ src/app/page/settings/restart/restart.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { DialogComponent } from '../../../shared/dialog/dialog.component';
 import { MatDialog } from '@angular/material';
 
