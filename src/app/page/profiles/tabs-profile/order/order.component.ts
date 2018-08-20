b50bbd0d5e69c7fe8521c0a19428b27f70482ec4
sumatokhinsa <c.c@bk.ru>
2018-08-20 13:02:23 +0300

    Подготовка таба заказов

--- src/app/page/profiles/tabs-profile/order/order.component.ts
+++ src/app/page/profiles/tabs-profile/order/order.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.styl']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() id: number;
 
-  ngOnInit() {
+  public orders = [ '123', '456', '789' ];
+
+  private isActive: boolean = true;
+
+  constructor( private orderService: OrderService) { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.isActive = false;
   }
 
 }
