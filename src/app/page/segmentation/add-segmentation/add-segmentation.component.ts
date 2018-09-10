a193b975d42613763c4079702943c95491534d64
sumatokhinsa <c.c@bk.ru>
2018-09-10 11:32:34 +0300

    Метод получения профилей и формы

--- src/app/page/segmentation/add-segmentation/add-segmentation.component.ts
+++ src/app/page/segmentation/add-segmentation/add-segmentation.component.ts
@@ -3,29 +3,64 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
 import * as _ from 'lodash';
 import { ActivatedRoute } from '@angular/router';
+import { takeWhile } from 'rxjs/operators';
+import { AddSegmentationService } from './add-segmentation.service';
 
-@Component({
+@Component( {
   selector: 'app-add-segmentation',
   templateUrl: './add-segmentation.component.html',
-  styleUrls: ['./add-segmentation.component.styl']
-})
+  styleUrls: [ './add-segmentation.component.styl' ]
+} )
 export class AddSegmentationComponent implements OnInit, OnDestroy {
 
-  private isActive: boolean;
   public formProfileSearch: FormGroup;
+  public buttonSave: boolean;
+  public buttonCreate: boolean;
+  public buttonSearch: boolean;
+
+  private isActive: boolean;
+  private profileId: number;
 
   constructor(
     private route: ActivatedRoute,
     private fb: FormBuilder,
+    private addSegmentationService: AddSegmentationService
   ) { }
 
   ngOnInit(): void {
     this.isActive = true;
-    this.initFormProfileSearch();
+    this.buttonSave = false;
+    this.buttonCreate = true;
+    this.buttonSearch = true;
+
+    this.formSegmentation();
+    this.formFilling();
   }
 
-  private initFormProfileSearch() {
-    this.formProfileSearch = this.fb.group({});
+  private initProfile() {
+
+  }
+
+  private formFilling() {
+    this.route.queryParams
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( params => {
+        if ( params.id ) {
+          this.buttonSave = true;
+          this.buttonCreate = false;
+          this.buttonSearch = false;
+
+          this.profileId = +params.id;
+          console.log( this.profileId );
+          this.addSegmentationService.getSegmentation( this.profileId ).subscribe( value => {
+            console.log( value );
+          } );
+        }
+      } );
+  }
+
+  private formSegmentation() {
+    // this.formSegmentation = this.fb.group( {} );
   }
 
   ngOnDestroy(): void {
--- src/app/page/segmentation/add-segmentation/add-segmentation.service.ts
+++ src/app/page/segmentation/add-segmentation/add-segmentation.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddSegmentationService {
 
-  constructor() { }
+  constructor(  private http: HttpClient ) { }
+
+  getSegmentation( id: number ): Observable<any> {
+    return this.http.get( `${environment.crmApi}/crm/segmentation/${id}/result`).pipe( retry( 10 ) );
+  }
+
 }
