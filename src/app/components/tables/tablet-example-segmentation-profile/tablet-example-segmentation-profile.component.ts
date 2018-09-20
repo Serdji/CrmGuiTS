0975ccbe6b41baa17cb3991c4592d92a28dc9926
sumatokhin <s.sumatokhin@sirena-travel.ru>
2018-09-20 15:52:44 +0300

    Сервис для таблице для ассинхронной пагинации

--- src/app/components/tables/tablet-example-segmentation-profile/tablet-example-segmentation-profile.component.ts
+++ src/app/components/tables/tablet-example-segmentation-profile/tablet-example-segmentation-profile.component.ts
@@ -10,6 +10,8 @@ import { timer } from 'rxjs/observable/timer';
 import { Router } from '@angular/router';
 import { SelectionModel } from '@angular/cdk/collections';
 import { takeWhile } from 'rxjs/operators';
+import { TabletExampleSegmentationProfileService } from './tablet-example-segmentation-profile.service';
+import { IpagPage } from '../../../interface/ipag-page';
 
 @Component( {
   selector: 'app-tablet-example-segmentation-profile',
@@ -23,8 +25,11 @@ export class TabletExampleSegmentationProfileComponent implements OnInit, OnDest
   public isCp: boolean = false;
   public selection = new SelectionModel<any>( true, [] );
   public isDisabled: boolean;
+  public resultsLength: number;
+  public isLoadingResults: boolean = false;
 
   private isActive: boolean;
+
 
   @Input() private tableDataSource: any;
 
@@ -34,12 +39,15 @@ export class TabletExampleSegmentationProfileComponent implements OnInit, OnDest
   constructor(
     private dialog: MatDialog,
     private router: Router,
+    private tabletExampleSegmentationProfileService: TabletExampleSegmentationProfileService
   ) { }
 
   ngOnInit(): void {
     this.isActive = true;
     this.initDataSource();
     this.initDisplayedColumns();
+    this.initPaginator();
+    this.initDataSourceAsync();
   }
 
   private initDisplayedColumns() {
@@ -53,6 +61,25 @@ export class TabletExampleSegmentationProfileComponent implements OnInit, OnDest
 
   private initDataSource() {
     this.dataSourceFun( this.tableDataSource );
+  }
+
+  private initPaginator() {
+    this.resultsLength = this.tabletExampleSegmentationProfileService.countPage;
+    this.paginator.page
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( ( value: IpagPage ) => {
+        this.tabletExampleSegmentationProfileService.setPagPage( value );
+        this.isLoadingResults = true;
+      } );
+  }
+
+  private initDataSourceAsync() {
+    this.tabletExampleSegmentationProfileService.subjectTableDataSource
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( ( value: any ) => {
+        this.dataSourceFun( value );
+        this.isLoadingResults = false;
+      } );
   }
 
   private dataSourceFun( params ) {
