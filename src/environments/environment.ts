a5f8b8c24bd8595cad491d344a6f5a1eda4b64c0
sumatokhinsa <c.c@bk.ru>
2018-09-26 13:32:50 +0300

    Откат теста

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,9 @@
-import { config } from 'assets/config';
-
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// `ng build --configuration=production"` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
   production: false,
-  crmApi: config.crmApi,
+  crmApi: 'https://10.1.8.155:8080/api2_dev',
 };
