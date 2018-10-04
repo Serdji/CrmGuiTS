e8fa6f5b6423267ad2a43616177a447cf4820521
sumatokhinsa <c.c@bk.ru>
2018-10-04 10:46:16 +0300

    Сохранения спец группы

--- src/app/page/profile-group/profile-group.component.ts
+++ src/app/page/profile-group/profile-group.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { ProfileGroupService } from './profile-group.service';
+import { takeWhile } from 'rxjs/operators';
+import * as _ from 'lodash';
+import { DialogComponent } from '../../shared/dialog/dialog.component';
+import { timer } from 'rxjs';
 
-@Component({
+@Component( {
   selector: 'app-profile-group',
   templateUrl: './profile-group.component.html',
-  styleUrls: ['./profile-group.component.styl']
-})
+  styleUrls: [ './profile-group.component.styl' ]
+} )
 export class ProfileGroupComponent implements OnInit {
 
-  constructor() { }
+  private isActive: boolean;
+  public formNameProfileGroup: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private dialog: MatDialog,
+    private profileGroupService: ProfileGroupService,
+  ) { }
+
+  ngOnInit(): void {
+    this.isActive = true;
+    this.initForm();
+  }
+
+  private initForm() {
+    this.formNameProfileGroup = this.fb.group( {
+      'CustomerGroupName': ''
+    } );
+  }
+
+  private resetForm() {
+    _( this.formNameProfileGroup.value ).each( ( value, key ) => {
+      this.formNameProfileGroup.get( key ).patchValue( '' );
+      this.formNameProfileGroup.get( key ).setErrors( null );
+    } );
+  }
+  private windowDialog( messDialog: string, params: string, disableTimer: boolean = false ) {
+    this.dialog.open( DialogComponent, {
+      data: {
+        message: messDialog,
+        status: params,
+      },
+    } );
+    if ( !disableTimer ) {
+      timer( 1500 )
+        .pipe( takeWhile( _ => this.isActive ) )
+        .subscribe( _ => {
+          this.dialog.closeAll();
+        } );
+    }
+  }
+
+  saveForm(): void {
+    const params = this.formNameProfileGroup.getRawValue();
+    this.profileGroupService.addProfileGroup( params )
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( () => {
+        this.windowDialog( `Спец группа успешно сохранена`, 'ok' );
+        this.resetForm();
+      } );
   }
 
 }
--- src/app/page/profile-group/profile-group.service.ts
+++ src/app/page/profile-group/profile-group.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../services/config-service.service';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileGroupService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService
+  ) { }
+
+  getProfileGroup( ): Observable<any> {
+    return this.http.get( `${this.configService.crmApi}/crm/customerGroup` ).pipe( retry( 10 ) );
+  }
+
+  addProfileGroup( params ): Observable<any> {
+    return this.http.post( `${this.configService.crmApi}/crm/customerGroup`,  params  ).pipe( retry( 10 ) );
+  }
+
 }
