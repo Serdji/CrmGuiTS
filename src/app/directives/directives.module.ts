09a14ca28c43a4ba5866b2725d3c19c63475875f
sumatokhinsa <c.c@bk.ru>
2018-10-05 13:19:00 +0300

    Пакет для скачки файла

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ConfirmEqualValidatorDirective } from './confirm-equal-validator.directive';
 import { AccessRightsDirective } from './access-rights.directive';
 import { OneRequiredDirective } from './one-required.directive';
-import { DownloadFileDirective } from './download-file.directive';
 
 @NgModule( {
   imports: [
@@ -13,13 +12,11 @@ import { DownloadFileDirective } from './download-file.directive';
     ConfirmEqualValidatorDirective,
     AccessRightsDirective,
     OneRequiredDirective,
-    DownloadFileDirective,
   ],
   exports: [
     ConfirmEqualValidatorDirective,
     AccessRightsDirective,
     OneRequiredDirective,
-    DownloadFileDirective,
   ]
 } )
 export class DirectivesModule {
