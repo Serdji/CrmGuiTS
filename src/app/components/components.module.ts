1eaa099e19afee8e5dfc41dbf2fa4c0184e3a74d
sumatokhinsa <c.c@bk.ru>
2018-10-29 11:26:43 +0300

    Установка editor и новый коммпонент

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,18 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { TablesModule } from './tables/tables.module';
 import { CommonModule } from '@angular/common';
+import { EditorModule } from './editor/editor.module';
 
 @NgModule({
   imports: [
     CommonModule,
     TablesModule,
+    EditorModule,
   ],
-  exports: [ TablesModule ],
+  exports: [
+    TablesModule,
+    EditorModule,
+  ],
 })
 export class ComponentsModule {
   static forRoot(): ModuleWithProviders {
