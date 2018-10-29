23591764aa083bf01e9498b0bd377b54f397b0ec
sumatokhinsa <c.c@bk.ru>
2018-10-29 17:28:05 +0300

    Компанет кнопки отправить сообщение

--- src/app/components/editors/editor/editor.component.ts
+++ src/app/components/editors/editor/editor.component.ts
@@ -1,26 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { NgxWigToolbarService, TButtonLibrary } from 'ngx-wig';
+import { NgxWigToolbarService } from 'ngx-wig';
 
-@Component({
+@Component( {
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: [ './editor.component.styl']
-})
-export class EditorComponent  implements OnInit {
+  styleUrls: [ './editor.component.styl' ]
+} )
+export class EditorComponent implements OnInit {
 
   @Input() ids: number[];
 
-  nameControl = new FormControl('Angular 5');
+  nameControl = new FormControl( 'Angular 7' );
 
 
-  constructor( private ngxWigToolbarService: NgxWigToolbarService) {
+  constructor( private ngxWigToolbarService: NgxWigToolbarService ) {
   }
 
-  ngOnInit() {
-    this.nameControl.valueChanges.subscribe(value => console.log(value));
-    console.log(this.ids);
-    console.log(this.ngxWigToolbarService.getToolbarButtons());
+  ngOnInit(): void {
+    this.nameControl.valueChanges.subscribe( value =>  console.log( value ) );
+    console.log( this.ids );
+    console.log( this.ngxWigToolbarService.getToolbarButtons() );
   }
 
 }
