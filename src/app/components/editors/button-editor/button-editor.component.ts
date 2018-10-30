dfa79ee4dbd5068998a3fef6855b783efb5d7401
sumatokhinsa <c.c@bk.ru>
2018-10-30 11:22:17 +0300

    Формирование параметров для отправки на сервер

--- src/app/components/editors/button-editor/button-editor.component.ts
+++ src/app/components/editors/button-editor/button-editor.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DialogEditorComponent } from '../dialog-editor/dialog-editor.component';
+import { ActivatedRoute } from '@angular/router';
+import { takeWhile } from 'rxjs/operators';
 
 @Component( {
   selector: 'app-button-editor',
   templateUrl: './button-editor.component.html',
   styleUrls: [ './button-editor.component.styl' ]
 } )
-export class ButtonEditorComponent implements OnInit {
+export class ButtonEditorComponent implements OnInit, OnDestroy {
 
-  constructor( public dialog: MatDialog ) { }
+  private isActive: boolean;
+
+  constructor(
+    public dialog: MatDialog,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.isActive = true;
   }
 
   openDialog(): void {
-    this.dialog.open( DialogEditorComponent, {
-      data: {
-        ids: [1, 2, 4]
-      }
-    } );
+    this.route.queryParams
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( value => {
+        this.dialog.open( DialogEditorComponent, {
+          data: {
+            params: value
+          }
+        } );
+      } );
+  }
+
+  ngOnDestroy(): void {
+    this.isActive = false;
   }
 
 }
--- src/app/components/editors/editor/editor.component.ts
+++ src/app/components/editors/editor/editor.component.ts
@@ -1,26 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxWigToolbarService } from 'ngx-wig';
+import { takeWhile } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Component( {
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: [ './editor.component.styl' ]
 } )
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy {
 
-  @Input() ids: number[];
+  @Input() params: any;
 
-  nameControl = new FormControl( 'Angular 7' );
+  public distribution: FormGroup;
+
+  private isActive: boolean;
 
 
-  constructor( private ngxWigToolbarService: NgxWigToolbarService ) {
+  constructor(
+    private ngxWigToolbarService: NgxWigToolbarService,
+    private fb: FormBuilder,
+  ) {
   }
 
   ngOnInit(): void {
-    this.nameControl.valueChanges.subscribe( value =>  console.log( value ) );
-    console.log( this.ids );
-    console.log( this.ngxWigToolbarService.getToolbarButtons() );
+    this.isActive = true;
+    this.initForm();
+
+  }
+
+  private initForm() {
+    this.distribution = this.fb.group( {
+      subject: [ '', [ Validators.required ] ],
+      text: '',
+      footer: [ '', [ Validators.required ] ],
+    }, {
+      updateOn: 'submit',
+    } );
+  }
+
+  sendDistribution(): void {
+    let newParams;
+    newParams = _.merge( this.distribution.getRawValue(), this.params );
+    console.log(newParams);
+  }
+
+  ngOnDestroy(): void {
+    this.isActive = false;
   }
 
 }
