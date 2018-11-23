26befab165021bc9bd3852c9336a99489797dd0a
sumatokhin <s.sumatokhin@sirena-travel.ru>
2018-11-23 21:24:06 +0300

    Возврат в заполенное поле поиска

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { filter, takeWhile } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { SidenavService } from '../layout/sidenav/sidenav.service';
@@ -13,12 +13,13 @@ import { IMenuLink } from '../../interface/imenu-link';
 export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
   private isActive: boolean;
-  private currentUrlArr: string[] = [];
-  private breadcrumbs: any[] = [];
+  private currentUrl: string;
+  private breadcrumbs: IMenuLink[] = [];
   private menuLink: IMenuLink[];
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private sidenavService: SidenavService
   ) { }
 
@@ -45,68 +46,71 @@ export class BreadcrumbsComponent implements OnInit, OnDestroy {
     }
   }
 
-  private initBreadcrumbs( currentUrlArr: string[] ) {
-    _.each( currentUrlArr, currentUrl => {
-      if ( _.size( _.chain( currentUrl ).split( '/' ).value() ) === 4 ) {
-        _.each( this.menuLink, link => {
-          if ( link.url === _.chain( currentUrl ).split( '/' ).take( 3 ).join( '/' ).value() ) {
-            this.breadcrumbs = _.take( this.breadcrumbs, 2 );
-            this.breadcrumbs.push(
-              {
-                url: currentUrl,
-                title: link.title,
-              }
-            );
-          }
-        } );
-      } else if ( _.size( _.chain( currentUrl ).split( '?' ).value() ) === 2 ) {
-        _.each( this.menuLink, link => {
-          if ( link.url === _.chain( currentUrl ).split( '?' ).take( 1 ).join( '/' ).value() ) {
-            this.breadcrumbs = _.take( this.breadcrumbs, 1 );
-            this.breadcrumbs.push(
-              {
-                url: currentUrl,
-                title: link.title,
-              }
-            );
-          }
-        } );
-      } else {
-        _.each( this.menuLink, link => {
-          if ( link.url === currentUrl ) {
-            this.breadcrumbs = _.take( this.breadcrumbs, 1 );
-            this.breadcrumbs.push(
-              {
-                url: currentUrl,
-                title: link.title
-              }
-            );
-          }
-        } );
-      }
-    } );
-    this.breadcrumbs = _.uniqBy( this.breadcrumbs, 'title' );
+  private initBreadcrumbs( currentUrl: string ) {
+    if ( _.size( _.chain( currentUrl ).split( '/' ).value() ) === 4 ) {
+      _.each( this.menuLink, link => {
+        if ( link.url === _.chain( currentUrl ).split( '/' ).take( 3 ).join( '/' ).value() ) {
+          this.breadcrumbs = _.take( this.breadcrumbs, 3 );
+          this.breadcrumbs.push(
+            {
+              url: currentUrl,
+              title: link.title,
+            }
+          );
+        }
+      } );
+    } else if ( _.size( _.chain( currentUrl ).split( '?' ).value() ) === 2 ) {
+      _.each( this.menuLink, link => {
+        if ( link.url === _.chain( currentUrl ).split( '?' ).take( 1 ).join( '/' ).value() ) {
+          this.breadcrumbs = _.take( this.breadcrumbs, 1 );
+          this.breadcrumbs.push(
+            {
+              url: currentUrl,
+              title: link.title,
+            }
+          );
+        }
+      } );
+    } else {
+      _.each( this.menuLink, link => {
+        if ( link.url === currentUrl ) {
+          this.breadcrumbs = _.take( this.breadcrumbs, 1 );
+          this.breadcrumbs.push(
+            {
+              url: currentUrl,
+              title: link.title
+            }
+          );
+        }
+      } );
+    }
+
+    this.breadcrumbs = _.chain( this.breadcrumbs )
+      .reverse()
+      .uniqBy( 'title' )
+      .reverse()
+      .value();
     localStorage.setItem( 'breadcrumbs', JSON.stringify( this.breadcrumbs ) );
   }
 
   private getBreadcrumbs() {
-    this.router.events.pipe(
-      takeWhile( _ => this.isActive ),
-      filter( event => event instanceof NavigationEnd )
-    ).subscribe( ( event: NavigationEnd ) => {
-      this.currentUrlArr.push( event.url );
-      this.initBreadcrumbs( this.currentUrlArr );
-    } );
-  }
-
-  private urlSplit( url: string ): string {
-    return _.chain( url )
-      .split( '/' )
-      .take( 3 )
-      .join( '/' )
-      .split( '?' )
-      .head()
-      .value();
+    this.router.events
+      .pipe(
+        takeWhile( _ => this.isActive ),
+        filter( event => event instanceof NavigationEnd )
+      )
+      .subscribe( ( event: NavigationEnd ) => {
+        this.currentUrl = '';
+        this.currentUrl = event.url;
+        this.initBreadcrumbs( this.currentUrl );
+      } );
+    this.route.queryParams
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( _ => {
+        this.currentUrl = '';
+        this.currentUrl = this.router.url;
+        this.initBreadcrumbs( this.currentUrl );
+      } );
   }
 
   ngOnDestroy(): void {
