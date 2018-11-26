4cea34d1ffd1281ae54b37999158d857c1325a3a
sumatokhinsa <c.c@bk.ru>
2018-11-26 15:48:24 +0300

    Начальная разметка странице добавления промокодов

--- src/app/page/promotions/add-promotions-cods/add-promotions-cods.component.ts
+++ src/app/page/promotions/add-promotions-cods/add-promotions-cods.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-promotions-cods',
   templateUrl: './add-promotions-cods.component.html',
   styleUrls: ['./add-promotions-cods.component.styl']
 })
-export class AddPromotionsCodsComponent implements OnInit {
+export class AddPromotionsCodsComponent implements OnInit, OnDestroy {
+
+  private isActive: boolean;
+
+  public isLoader: boolean;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.isActive = true;
+    this.isLoader = true;
+  }
+
+  saveForm(): void {
+
+  }
+
+  clearForm(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.isActive = false;
   }
 
 }
