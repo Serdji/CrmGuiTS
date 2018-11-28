eb3189c11aab78876e0d8eaed9dbd2f29d7d1f1d
sumatokhinsa <c.c@bk.ru>
2018-11-28 10:08:54 +0300

    Сохранения промокода на сервере

--- src/app/page/promotions/add-promotions/add-promotions.component.ts
+++ src/app/page/promotions/add-promotions/add-promotions.component.ts
@@ -1,27 +1,59 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AddPromotionsService } from './add-promotions.service';
+import { takeWhile } from 'rxjs/operators';
 
-@Component({
+@Component( {
   selector: 'app-add-promotions',
   templateUrl: './add-promotions.component.html',
-  styleUrls: ['./add-promotions.component.styl']
-})
+  styleUrls: [ './add-promotions.component.styl' ]
+} )
 export class AddPromotionsComponent implements OnInit, OnDestroy {
+
+  public isLoader: boolean;
+  public formPromotions: FormGroup;
 
   private isActive: boolean;
 
-  public isLoader: boolean;
-
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private addPromotionsService: AddPromotionsService
+  ) { }
 
   ngOnInit(): void {
     this.isActive = true;
     this.isLoader = true;
+    this.initForm();
   }
 
-  saveForm(): void {}
+  private initForm() {
+    this.formPromotions = this.fb.group( {
+      promotionName: ''
+    } );
+  }
+
+  saveForm(): void {
+    this.addPromotionsService.saveSegmentation( this.formPromotions.getRawValue() )
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe();
+  }
 
   ngOnDestroy(): void {
     this.isActive = false;
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/page/promotions/add-promotions/add-promotions.service.ts
+++ src/app/page/promotions/add-promotions/add-promotions.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ConfigService } from '../../../services/config-service.service';
 import { RetryRequestService } from '../../../services/retry-request.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,9 @@ export class AddPromotionsService {
     private configService: ConfigService,
     private retryRequestService: RetryRequestService
   ) { }
+
+  saveSegmentation( params ): Observable<any> {
+    return this.http.post( this.configService.crmApi + '/crm/promotions', params ).pipe( this.retryRequestService.retry() );
+  }
+
 }
