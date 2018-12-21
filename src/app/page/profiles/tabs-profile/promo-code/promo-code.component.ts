bd5beaa97d6fe5b5129050a5516103de5be2f184
sumatokhinsa <c.c@bk.ru>
2018-12-21 14:30:53 +0300

    Метод получения всех промокодов

--- src/app/page/profiles/tabs-profile/promo-code/promo-code.component.ts
+++ src/app/page/profiles/tabs-profile/promo-code/promo-code.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnDestroy, OnInit, Input } from '@angular/core';
+import { IPromoCode } from '../../../../interface/ipromo-code';
+import { PromoCodeService } from './promo-code.service';
+import { takeWhile } from 'rxjs/operators';
 
-@Component({
+@Component( {
   selector: 'app-promo-code',
   templateUrl: './promo-code.component.html',
-  styleUrls: ['./promo-code.component.styl']
-})
+  styleUrls: [ './promo-code.component.styl' ]
+} )
 export class PromoCodeComponent implements OnInit, OnDestroy {
 
   @Input() id: number;
 
   public progress: boolean;
+  public promoCodes: IPromoCode;
 
   private isActive: boolean;
 
-  constructor() { }
+  constructor( private promoCodeService: PromoCodeService ) { }
 
   ngOnInit(): void {
     this.isActive = true;
     this.progress = false;
+    this.initPromoCodes();
+  }
+
+  private initPromoCodes() {
+    this.promoCodeService.getPromoCodes( { 'customerId': this.id } )
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( ( promoCodes: IPromoCode ) => this.promoCodes = promoCodes );
   }
 
   ngOnDestroy(): void {
--- src/app/page/profiles/tabs-profile/promo-code/promo-code.service.ts
+++ src/app/page/profiles/tabs-profile/promo-code/promo-code.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../../../services/config-service.service';
+import { RetryRequestService } from '../../../../services/retry-request.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromoCodeService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService,
+    private retryRequestService: RetryRequestService,
+  ) { }
+
+  getPromoCodes( params ): Observable<any> {
+    return this.http.get( `${this.configService.crmApi}/crm/promoCodes/findUsableForCustomer`, { params } ).pipe( this.retryRequestService.retry() );
+  }
+
 }
