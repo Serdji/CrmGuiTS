f0f142d8c2953ede3faac044491e541f84c653e5
sumatokhinsa <c.c@bk.ru>
2019-02-05 12:43:03 +0300

    финиш

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './page/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
-import { ProfileSearchComponent } from './page/profiles/profile-search/profile-search.component';
 import { LayoutComponent } from './shared/layout/layout.component';
-import { AddUserComponent } from './page/users/add-user/add-user.component';
-import { EntranceComponent } from './page/entrance/entrance.component';
-import { ListUsersComponent } from './page/users/list-users/list-users.component';
-import { UserComponent } from './page/users/user/user.component';
-import { AddProfileComponent } from './page/profiles/add-profile/add-profile.component';
 import { AccessRightsUserGuard } from './guards/access-rights-user.guard';
 import { AccessRightsProfileGuard } from './guards/access-rights-profile.guard';
-import { FormTableAsyncProfileSettingsComponent } from './page/settings/form-table-async-profile-settings/form-table-async-profile-settings.component';
-import { TabsProfileComponent } from './page/profiles/tabs-profile/tabs-profile.component';
-import { RestartComponent } from './page/settings/restart/restart.component';
-import { AddSegmentationComponent } from './page/segmentation/add-segmentation/add-segmentation.component';
-import { ListSegmentationComponent } from './page/segmentation/list-segmentation/list-segmentation.component';
-import { ProfileGroupComponent } from './page/special-groups/profile-group/profile-group.component';
-import { ErrorPageComponent } from './page/error-page/error-page.component';
-import { ListDistributionComponent } from './page/distribution/list-distribution/list-distribution.component';
-import { ProfileDistributionComponent } from './page/distribution/profile-distribution/profile-distribution.component';
 import { AccessRightsDistributionGuard } from './guards/access-rights-distribution.guard';
-import { AddPromotionsComponent } from './page/promotions/add-promotions/add-promotions.component';
-import { AddPromotionsCodesComponent } from './page/promotions/add-promotions-codes/add-promotions-codes.component';
-import { SearchPromotionsCodesComponent } from './page/promotions/search-promotions-codes/search-promotions-codes.component';
 
 const routes: Routes = [
     { path: '', loadChildren: './page/login/login.module#LoginModule' },
@@ -51,8 +32,8 @@ const routes: Routes = [
         { path: 'restart', loadChildren: './page/settings/restart/restart.module#RestartModule' },
       ],
     },
-  // { path: '404', component: ErrorPageComponent },
-  // { path: '**', redirectTo: '404', pathMatch: 'full' }
+  { path: '404', loadChildren: './page/error-page/error-page.module#ErrorPageModule' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' }
   ]
 ;
 
--- src/app/page/error-page/error-page.module.ts
+++ src/app/page/error-page/error-page.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ErrorPageComponent } from './error-page.component';
 import { SharedModule } from '../../shared/shared.module';
 import { MaterialModule } from '../../shared/material';
+import { ErrorPageRoutes } from './error-page.routing';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     MaterialModule,
+    ErrorPageRoutes
   ],
   declarations: [ErrorPageComponent]
 })
