3da20a2721f7c47061c7279ee00f1a832643a572
sumatokhinsa <c.c@bk.ru>
2019-02-12 12:21:52 +0300

    Вывод статусов в подтаблицу

--- src/app/components/tables/table-example-distribution/table-example-distribution.component.ts
+++ src/app/components/tables/table-example-distribution/table-example-distribution.component.ts
@@ -19,7 +19,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   styleUrls: [ './table-example-distribution.component.styl' ],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none', borderColor: 'rgba(0,0,0,0)'})),
       state('expanded', style({height: '*'})),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
