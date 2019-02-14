022f23595afb1b20f6148870001f6345bde3bddf
sumatokhinsa <c.c@bk.ru>
2019-02-14 14:04:22 +0300

    Декаомпазиция

--- src/app/page/reports/distribution-report/distribution-report.component.ts
+++ src/app/page/reports/distribution-report/distribution-report.component.ts
@@ -1,29 +1,37 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-@Component({
+@Component( {
   selector: 'app-distribution-report',
   templateUrl: './distribution-report.component.html',
-  styleUrls: ['./distribution-report.component.styl']
-})
+  styleUrls: [ './distribution-report.component.styl' ]
+} )
 export class DistributionReportComponent implements OnInit, OnDestroy {
 
   private isActive: boolean;
 
-  isLinear = false;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  public isLinear = false;
+  public templateForm: FormGroup;
+  public dynamicForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
     this.isActive = true;
-    this.firstFormGroup = this.fb.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this.fb.group({
-      secondCtrl: ['', Validators.required]
-    });
+    this.initTemplateForm();
+    this.initDynamicForm();
+  }
+
+  private initTemplateForm() {
+    this.templateForm = this.fb.group( {
+      firstCtrl: [ '', Validators.required ]
+    } );
+  }
+
+  private initDynamicForm() {
+    this.dynamicForm = this.fb.group( {
+      secondCtrl: [ '', Validators.required ]
+    } );
   }
 
   ngOnDestroy(): void {
