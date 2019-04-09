577f01a4065334f85c9d79fe2195595a9bf0b240
sumatokhinsa <c.c@bk.ru>
2019-04-09 14:34:48 +0300

    Верста таба льготы

--- src/app/page/profiles/tabs-profile/privileges/privileges.component.ts
+++ src/app/page/profiles/tabs-profile/privileges/privileges.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import * as R from 'ramda';
 
 @Component({
   selector: 'app-privileges',
@@ -7,14 +8,31 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 })
 export class PrivilegesComponent implements OnInit, OnDestroy {
 
+  public progress: boolean;
+
   private isActive: boolean;
+  private isSortFilterReverse: boolean;
 
   @Input() id: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.progress = true;
+    this.isSortFilterReverse = false;
     this.isActive = true;
+  }
+
+  sortFilter( title: string ): void {
+    const isSortFilterReverse = _ => this.isSortFilterReverse = !this.isSortFilterReverse;
+    const isSortFilterReverseFunc = R.ifElse( isSortFilterReverse, R.identity, R.reverse );
+    const sortByTitle = R.compose( R.sortBy, R.path, R.split( '.' ) );
+    const funcSortByTitle = R.compose(
+      isSortFilterReverseFunc,
+      sortByTitle( title )
+    );
+
+    // this.promoCodes.result = funcSortByTitle( this.promoCodes.result );
   }
 
   ngOnDestroy(): void {
