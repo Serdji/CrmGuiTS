f9a544cea5134360b32c5fbfcbccc13e4c67c5e2
sumatokhinsa <c.c@bk.ru>
2019-04-11 11:19:00 +0300

    Новый интерфейс для объекта льгот

--- src/app/page/profiles/tabs-profile/privileges/privileges.component.ts
+++ src/app/page/profiles/tabs-profile/privileges/privileges.component.ts
@@ -1,29 +1,41 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import * as R from 'ramda';
+import { PrivilegesService } from './privileges.service';
+import { takeWhile } from 'rxjs/operators';
+import { IPrivileges } from '../../../../interface/iprivileges';
 
-@Component({
+@Component( {
   selector: 'app-privileges',
   templateUrl: './privileges.component.html',
-  styleUrls: ['./privileges.component.styl']
-})
+  styleUrls: [ './privileges.component.styl' ]
+} )
 export class PrivilegesComponent implements OnInit, OnDestroy {
 
   public progress: boolean;
+  public privileges: IPrivileges;
 
   private isActive: boolean;
   private isSortFilterReverse: boolean;
 
   @Input() id: number;
 
-  constructor() { }
+  constructor( private privilegesService: PrivilegesService ) { }
 
   ngOnInit(): void {
     this.progress = true;
     this.isSortFilterReverse = false;
     this.isActive = true;
+    this.initPrivileges();
   }
 
-  sortFilter( title: string ): void {
+  private initPrivileges() {
+    const success = ( value: IPrivileges ) => this.privileges = value;
+    this.privilegesService.getPrivileges( this.id )
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( success );
+  }
+
+  onSortFilter( title: string ): void {
     const isSortFilterReverse = _ => this.isSortFilterReverse = !this.isSortFilterReverse;
     const isSortFilterReverseFunc = R.ifElse( isSortFilterReverse, R.identity, R.reverse );
     const sortByTitle = R.compose( R.sortBy, R.path, R.split( '.' ) );
@@ -32,7 +44,7 @@ export class PrivilegesComponent implements OnInit, OnDestroy {
       sortByTitle( title )
     );
 
-    // this.promoCodes.result = funcSortByTitle( this.promoCodes.result );
+    this.privileges.result = funcSortByTitle( this.privileges.result );
   }
 
   ngOnDestroy(): void {
--- src/app/page/profiles/tabs-profile/privileges/privileges.service.ts
+++ src/app/page/profiles/tabs-profile/privileges/privileges.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '../../../../services/config-service.service';
+import { RetryRequestService } from '../../../../services/retry-request.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrivilegesService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private configService: ConfigService,
+    private retryRequestService: RetryRequestService
+  ) { }
+
+  getPrivileges( id: number ): Observable<any> {
+    return this.http.get( this.configService.crmApi + `/crm/customer/${id}/discounts` ).pipe( this.retryRequestService.retry() );
+  }
+
 }
