2bb9c01bd22feee63cbf556efe1e53ac7f32afe8
sumatokhinsa <c.c@bk.ru>
2019-08-09 16:25:37 +0300

    Пятница

--- src/app/components/editors/editor-sms/editor-sms.component.ts
+++ src/app/components/editors/editor-sms/editor-sms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-editor-sms',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editor-sms.component.styl']
 })
 export class EditorSmsComponent implements OnInit {
+
+  @Input() params: any;
+  @Input() totalCount: number;
+
 
   constructor() { }
 
