225faace5a1c4cc65d913dac312819260365c4d7
sumatokhinsa <c.c@bk.ru>
2019-08-09 15:40:22 +0300

    Новый компонент для sms рассылки

---
+++ src/app/components/editors/editor-sms/editor-sms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-sms',
+  templateUrl: './editor-sms.component.html',
+  styleUrls: ['./editor-sms.component.styl']
+})
+export class EditorSmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
