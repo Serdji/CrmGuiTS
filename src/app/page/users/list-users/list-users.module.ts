86cd5bae78b126317e9918a9503bbf8f0ae93e63
sumatokhinsa <c.c@bk.ru>
2019-09-23 10:33:19 +0300

    Список пользователей

--- src/app/page/users/list-users/list-users.module.ts
+++ src/app/page/users/list-users/list-users.module.ts
@@ -5,13 +5,15 @@ import { SharedModule } from '../../../shared/shared.module';
 import { ListUsersService } from './list-users.service';
 import { ComponentsModule } from '../../../components/components.module';
 import { ListUsersRoutes } from './list-users.routing';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule( {
   imports: [
     CommonModule,
     SharedModule,
     ComponentsModule,
-    ListUsersRoutes
+    ListUsersRoutes,
+    TranslateModule
   ],
   declarations: [ ListUsersComponent ],
   providers: [ ListUsersService ]
