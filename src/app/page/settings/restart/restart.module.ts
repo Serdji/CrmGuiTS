4c3e72726b8c7fc7d3d2585e7bc17210f85c1d34
sumatokhinsa <c.c@bk.ru>
2019-09-26 11:54:09 +0300

    Перезагрузка

--- src/app/page/settings/restart/restart.module.ts
+++ src/app/page/settings/restart/restart.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RestartComponent } from './restart.component';
 import { SharedModule } from '../../../shared/shared.module';
 import { RestartRoutes } from './restart.routing';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    RestartRoutes
+    RestartRoutes,
+    TranslateModule
   ],
   declarations: [RestartComponent]
 })
