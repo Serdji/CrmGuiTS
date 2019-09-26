82a225054c783a7d8a53241325162de884bc80de
sumatokhinsa <c.c@bk.ru>
2019-09-26 11:51:54 +0300

    Настройка таблице профилей

--- src/app/pipes/field-translation.pipe.ts
+++ src/app/pipes/field-translation.pipe.ts
@@ -7,13 +7,13 @@ export class FieldTranslationPipe implements PipeTransform {
 
   transform(value: string): string {
     switch ( value ) {
-      case  'firstName': return 'Имя';
-      case  'lastName': return 'Фамилия';
-      case  'secondName': return 'Отчество';
+      case  'firstName': return 'PAGE.PROFILES.SEARCH_PROFILE.TABLE.FIRST_NAME';
+      case  'lastName': return 'PAGE.PROFILES.SEARCH_PROFILE.TABLE.LAST_NAME';
+      case  'secondName': return 'PAGE.PROFILES.SEARCH_PROFILE.TABLE.MIDDLE_NAME';
       case  'prefix': return 'Префикс';
-      case  'gender': return 'Пол';
-      case  'dob': return 'Дата рождения';
-      case  'customerIds': return '№ профиля';
+      case  'gender': return 'PAGE.PROFILES.SEARCH_PROFILE.TABLE.GENDER';
+      case  'dob': return 'PAGE.PROFILES.SEARCH_PROFILE.TABLE.DATE_OF_BIRTH';
+      case  'customerIds': return 'PAGE.PROFILES.SEARCH_PROFILE.TABLE.CUSTOMER_IDS';
       case  'Active': return 'PAGE.PROFILES.TABS_PROFILE.ORDER.EXPANSION.PANEL_HEADER.STATUS.ACTIVE';
       case  'Cancelled': return 'PAGE.PROFILES.TABS_PROFILE.ORDER.EXPANSION.PANEL_HEADER.STATUS.CANCELLED';
       case  'Amount': return 'Сумма';
