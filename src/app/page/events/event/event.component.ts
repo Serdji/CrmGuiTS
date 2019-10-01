e04fc08efd4272fb8de334926d299f1a48e8760c
sumatokhinsa <c.c@bk.ru>
2019-10-01 11:31:19 +0300

    Верска и вывод данных на страницу события

--- src/app/page/events/event/event.component.ts
+++ src/app/page/events/event/event.component.ts
@@ -2,6 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { takeWhile } from 'rxjs/operators';
 import { EventService } from './event.service';
+import { ITask } from '../../../interface/itask';
+import { ListEventService } from '../list-event/list-event.service';
+import * as R from 'ramda';
+import * as moment from 'moment';
+import { TranslateService } from '@ngx-translate/core';
+import { forkJoin } from 'rxjs';
 
 @Component( {
   selector: 'app-event',
@@ -10,16 +16,22 @@ import { EventService } from './event.service';
 } )
 export class EventComponent implements OnInit, OnDestroy {
 
+  public task: ITask;
+  public isProgress: boolean;
+
   private taskId: number;
   private isActive: boolean;
 
   constructor(
     private route: ActivatedRoute,
-    private eventService: EventService
+    private translate: TranslateService,
+    private eventService: EventService,
+    private listEventService: ListEventService
   ) { }
 
   ngOnInit() {
     this.isActive = true;
+    this.isProgress = true;
     this.initQueryRouter();
   }
 
@@ -27,19 +39,40 @@ export class EventComponent implements OnInit, OnDestroy {
     this.route.params
       .pipe( takeWhile( _ => this.isActive ) )
       .subscribe( params => {
-        this.taskId = params.id;
+        this.taskId = +params.id;
+        this.initTask( this.taskId );
       } );
   }
 
-  // trigger(): void {
-  //   const send = {
-  //     TaskId: this.task.taskId,
-  //     IsActive: true
-  //   };
-  //   this.eventService.tackActivate( send )
-  //     .pipe( takeWhile( _ => this.isActive ) )
-  //     .subscribe();
-  // }
+  private initTask( id: number ) {
+    const success = ( value ) => {
+      const tasks: ITask[] = value[ 0 ];
+      const frequencySecLens = R.lensProp( 'frequencySec' );
+      this.task = R.find( R.propEq( 'taskId', id ), tasks );
+      this.task = R.set( frequencySecLens, moment.duration( { 'second': this.task.frequencySec } ).locale( this.translate.store.currentLang ).humanize(), this.task );
+      this.isProgress = false;
+      console.log( this.task );
+    };
+
+    const listEventService = this.listEventService.getAllTasks();
+    const translate = this.translate.get( 'MENU' );
+    const servicesForkJoin = forkJoin( [ listEventService, translate ] );
+
+    servicesForkJoin
+      .pipe( takeWhile( _ => this.isActive ) )
+      .subscribe( success );
+  }
+
+  trigger(): void {
+    const send = {
+      TaskId: this.taskId,
+      IsActive: true
+    };
+    console.log( send );
+    // this.eventService.tackActivate( send )
+    //   .pipe( takeWhile( _ => this.isActive ) )
+    //   .subscribe();
+  }
 
   ngOnDestroy(): void {
     this.isActive = false;
