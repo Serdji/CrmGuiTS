c217ced26b52702989694dd8193003976f8f72f3
sumatokhinsa <c.c@bk.ru>
2019-11-21 11:17:01 +0300

    Англоязычный перевод

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,11 +56,11 @@ export class AppComponent implements OnInit {
   }
 
   private initTranslate() {
-    this.translate.addLangs( [ 'ru', 'en', 'de', 'ja', 'zh', 'cs' ] );
+    this.translate.addLangs( [ 'ru', 'en' ] );
     this.translate.setDefaultLang( 'ru' );
     this.translate.use('ru' );
     const browserLang = this.translate.getBrowserLang();
-    this.translate.use( browserLang.match( /ru|en|'de|ja|zh|cs'/ ) ? browserLang : 'ru' );
+    this.translate.use( browserLang.match( /ru|en'/ ) ? browserLang : 'en' );
     this.translate.stream( 'MENU' ).subscribe( _ => {
       this._adapter.setLocale( this.translate.store.currentLang );
     } );
