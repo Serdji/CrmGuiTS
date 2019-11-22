dedde837b00ce8b3926d310e926b47ddb38c90db
sumatokhinsa <c.c@bk.ru>
2019-11-22 11:01:29 +0300

    Фикс, только Русского языка

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,11 +56,10 @@ export class AppComponent implements OnInit {
   }
 
   private initTranslate() {
-    this.translate.addLangs( [ 'ru', 'en', 'de', 'ja', 'zh', 'cs' ] );
+    this.translate.addLangs( [ 'ru' ] );
     this.translate.setDefaultLang( 'ru' );
-    this.translate.use('ru' );
     const browserLang = this.translate.getBrowserLang();
-    this.translate.use( browserLang.match( /ru|en|'de|ja|zh|cs'/ ) ? browserLang : 'ru' );
+    // this.translate.use( browserLang.match( /ru|en'/ ) ? browserLang : 'ru' );
     this.translate.stream( 'MENU' ).subscribe( _ => {
       this._adapter.setLocale( this.translate.store.currentLang );
     } );
