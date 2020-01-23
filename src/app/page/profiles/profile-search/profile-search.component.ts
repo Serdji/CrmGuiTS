174fd3c84563cca0c8588088c76ece2e4be9af27
sumatokhinsa <c.c@bk.ru>
2020-01-23 15:35:33 +0300

    поле craft

--- src/app/page/profiles/profile-search/profile-search.component.ts
+++ src/app/page/profiles/profile-search/profile-search.component.ts
@@ -27,6 +27,7 @@ import { IAirlineLCode } from '../../../interface/iairline-lcode';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { ISellType } from '../../../interface/isell-type';
 import { ICountries } from '../../../interface/icountries';
+import { logger } from 'codelyzer/util/logger';
 
 @Component( {
   selector: 'app-profile-search',
@@ -257,7 +258,7 @@ export class ProfileSearchComponent implements OnInit, OnDestroy {
       airlineLCode: '',
       sellType: '',
       sellCountry: '',
-      AircraftType: '',
+      craft: '',
       flight: '',
       flightdatefrom: '',
       flightdateto: '',
@@ -280,7 +281,6 @@ export class ProfileSearchComponent implements OnInit, OnDestroy {
     } );
     this.switchCheckbox();
     this.forkJoinObservable();
-    this.formProfileSearch.get('AircraftType').disable();
   }
 
   private switchCheckbox() {
@@ -365,6 +365,8 @@ export class ProfileSearchComponent implements OnInit, OnDestroy {
     const segmentation = [];
     const customerGroup = [];
 
+    console.log( this.formProfileSearch.value );
+
     for ( const segmentationChip of this.segmentationChips ) {
       if ( segmentationChip ) {
         segmentation.push( _.chain( this.segmentation ).find( { 'title': segmentationChip } ).get( 'segmentationId' ).value() );
@@ -378,7 +380,10 @@ export class ProfileSearchComponent implements OnInit, OnDestroy {
     }
 
     for ( const key of formValue ) {
-      const valueForm = keys => this.formProfileSearch.get( keys ).value || '';
+      const valueForm = keys => {
+        console.log( keys, this.formProfileSearch.get( keys ).value );
+        return this.formProfileSearch.get( keys ).value || '';
+      }
       if ( this.isKeys( key, 'all' ) ) highlightObj[ key ] = valueForm( key ).trim();
       if ( this.isKeys( key, 'data' ) ) highlightObj[ key ] = moment( valueForm( key ) ).format( 'DD.MM.YYYY' );
       if ( this.isKeys( key, 'checkbox' ) ) {
@@ -460,6 +465,7 @@ export class ProfileSearchComponent implements OnInit, OnDestroy {
           && key !== 'airlineLCode'
           && key !== 'sellCountry'
           && key !== 'sellType'
+          && key !== 'craft'
           && key !== 'deptimefrominclude'
           && key !== 'deptimetoexclude'
           && key !== 'dobfrominclude'
