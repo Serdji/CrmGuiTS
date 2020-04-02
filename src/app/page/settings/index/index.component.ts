5f4c615ae4978d27e6a2b4f6733acf20a62fef4d
sumatokhin <s.sumatokhin@sirena-travel.ru>
2020-04-02 10:40:51 +0300

    Сообщение об успешном изменение индекса

--- src/app/page/settings/index/index.component.ts
+++ src/app/page/settings/index/index.component.ts
@@ -1,25 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { IndexService } from './index.service';
 import { IindexConfig } from '../../../interface/iindex-config';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../../shared/dialog/dialog.component';
+import { timer } from 'rxjs/observable/timer';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { tap } from 'rxjs/operators';
 
-@Component({
+@Component( {
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.styl']
-})
-export class IndexComponent implements OnInit {
+  styleUrls: [ './index.component.styl' ]
+} )
+export class IndexComponent implements OnInit, OnDestroy {
+
+  public isDisabledButton: boolean;
 
   private formIndex: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private indexService: IndexService
+    private indexService: IndexService,
+    private dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
+    this.isDisabledButton = false;
     this.initFormIndex();
     this.initGetIndexConfig();
+  }
+
+  private windowDialog( messDialog: string, params: string ) {
+    this.dialog.open( DialogComponent, {
+      data: {
+        message: messDialog,
+        status: params,
+      },
+    } );
+    timer( 1500 )
+      .pipe( untilDestroyed( this ) )
+      .subscribe( _ => {
+        this.dialog.closeAll();
+      } );
   }
 
   initFormIndex() {
@@ -33,14 +56,23 @@ export class IndexComponent implements OnInit {
     } );
   }
 
-  private  initGetIndexConfig() {
-    this.indexService.getIndexConfig().subscribe( (config: IindexConfig) => {
+  private initGetIndexConfig() {
+    this.indexService.getIndexConfig().subscribe( ( config: IindexConfig ) => {
       this.formIndex.patchValue( config );
     } );
   }
 
   public onEditIndexConfig(): void {
-    this.indexService.putIndexConfig( this.formIndex.value ).subscribe();
+    this.isDisabledButton = true;
+    this.indexService.putIndexConfig( this.formIndex.value )
+      .pipe( tap( _ => this.isDisabledButton = false ) )
+      .subscribe(
+      ( config: IindexConfig ) => {
+        this.formIndex.patchValue( config );
+        this.windowDialog( 'DIALOG.OK.INDEX_SETTING', 'ok' );
+      } );
   }
+
+  ngOnDestroy(): void {}
 
 }
