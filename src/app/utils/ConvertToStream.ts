66634a9dd7939ea027bd2a3574df8b74804b7501
sumatokhin <s.sumatokhin@sirena-travel.ru>
2020-04-20 11:51:49 +0300

    Адаптация EMAIL рассылки и инжектирование ультелиты

--- src/app/utils/ConvertToStream.ts
+++ src/app/utils/ConvertToStream.ts
@@ -1,8 +1,11 @@
 import { Observable } from 'rxjs';
 import { concatAll, toArray } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class ConvertToStream {
   public stream = ( ...operators: Function[] ) => this.pipeFromArray( concatAll(), ...operators, toArray() );
-  private pipeFromArray = ( ...operators: Function[] ) => ( source: Observable<any> ) => operators.reduce( ( acc, fn ) => fn( acc ), source ); 1
-
+  private pipeFromArray = ( ...operators: Function[] ) => ( source: Observable<any> ) => operators.reduce( ( acc, fn ) => fn( acc ), source );
 }
