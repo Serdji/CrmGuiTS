db0f7607398492ecb4ce96f9dc904f40e292fc78
sumatokhin <s.sumatokhin@sirena-travel.ru>
2020-05-21 15:16:38 +0300

    Добавление реактивной формы

--- src/app/page/settings/age-interval/age-interval.component.ts
+++ src/app/page/settings/age-interval/age-interval.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { IndexService } from '../index/index.service';
+import { MatDialog } from '@angular/material/dialog';
 
-@Component({
+@Component( {
   selector: 'app-age-interval',
   templateUrl: './age-interval.component.html',
-  styleUrls: ['./age-interval.component.styl']
-})
+  styleUrls: [ './age-interval.component.styl' ]
+} )
 export class AgeIntervalComponent implements OnInit {
+  public formAgeInterval: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private indexService: IndexService,
+    private dialog: MatDialog,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.initForm();
   }
+
+  private initForm(): void {
+    this.formAgeInterval = this.fb.group( {
+      ageTo: '',
+      ageFrom: '',
+      title: ''
+    } );
+  }
+
 
 }
